7a2f41a38a8f530bad4c61419be20011431114b9
Poluux <hugoboss82001@hotmail.com>
2025-04-10 12:42:58 +0200

    started to write the data context

--- DataAccessLayer/DataContext.cs
+++ DataAccessLayer/DataContext.cs
@@ -1,7 +1,18 @@
-﻿namespace DataAccessLayer
-{
-    public class Class1
-    {
+﻿using Microsoft.EntityFrameworkCore;
 
+namespace DataAccessLayer
+{
+    public class DataContext : DbContext
+    {
+        public DataContext()
+        {
+
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            
+
+        }
     }
 }
